39e578695a0cbccebb59eb52b2d5353c56d3751a
pravin.shelar@synechron.com <pravin.shelar@synechron.com>
2020-08-12 21:06:28 -0400

    Security - helmet, rate limiting, sanitize, etc

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
-var createError = require('http-errors');
-var path = require('path');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 var logger = require('morgan');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -15,24 +18,43 @@ const app = express();
 
 app.use(logger('dev'));
 
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Enable CORS
+app.use(cors());
+
 // Body parser
 app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, DELETE, OPTIONS"
+//   );
+//   next();
+// });
 
 
 // app.use(function (req, res, next) {
@@ -62,7 +84,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 
 app.listen(
-  PORT, 
+  PORT,
   console.log(
     `RESTful API server started on port ${PORT}`
   )
