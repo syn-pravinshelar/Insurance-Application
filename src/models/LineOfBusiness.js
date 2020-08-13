031544b3a261168616266ce1047900edd0992ed4
Ravijeet Udeykar <ravijitpcce@gmail.com>
2020-08-12 23:13:42 -0400

    fixing this upper case file convention

--- src/models/LineOfBusiness.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var limitScehma = new mongoose.Schema({
- lineofbusiness: mongoose.Schema.Types.String
-})
-
-const LineOfBusiness = mongoose.model("LineOfBusiness",limitScehma);
-module.exports = LineOfBusiness;
