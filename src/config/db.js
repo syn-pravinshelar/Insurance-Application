ae4d402877f9976180210580b5bd02c4aca576cd
Ravijeet Udeykar <ravijitpcce@gmail.com>
2020-08-15 13:37:49 -0400

    Health Route is now checking if mongo db is running or not

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-// Mongo DB Atalas connection string-
-// mongoose.connect('mongodb+srv://pravin:elle73IgmaGAaE3P@cluster0.xuqex.mongodb.net/insurance-app?retryWrites=true&w=majority', {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-//   })
-
 const connectDB = async () => {
   return mongoose.connect('mongodb://mongo:27017/synechron', {
     useNewUrlParser: true,
